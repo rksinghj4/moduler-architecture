95edbad3ae76e7a1e614359b5359dc30ceda2e94
Raj Kumar Singh <rksinghj4@gmail.com>
2025-08-04 17:58:25 +0530

    Add initial setup

---
+++ ModulerArchitectureDemo/app/build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.raj.modulerarchitecture"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.raj.modulerarchitecture"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    //
+    implementation(libs.squareup.retrofit2)
+    //Gson converter
+    implementation(libs.squareup.retrofit2.converter.gson)
+    implementation(libs.dagger.hilt)
+    //To import androidx.hilt.navigation.compose.hiltViewModel
+    implementation(libs.androidx.hilt.navigation.compose)
+    ksp(libs.dagger.hilt.android.compiler)
+    //To import coil.compose.AsyncImage, coil3 has multiplatform support
+    implementation(libs.coil.compose)
+    //To import androidx.browser.customtabs.CustomTabsIntent
+    implementation(libs.androidx.browser)
+    implementation(libs.androidx.navigation)    //Navigation compose
+    //In Navigation - compose used for data class serialization/deserialization
+    implementation(libs.kotlinx.serialization.json)
+
+// Kotlinx Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+// Retrofit converter
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+    debugImplementation("io.nerdythings:okhttp-profiler:1.1.1")
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.test.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
